5c1f3b1402cd8041b08ba86f4ec9a703debfe280
Frederik Riedel <frederik.riedel@frogg.io>
2017-04-09 14:12:48 +0200

    added correct event url

--- World Wide Protest/FirstViewController.h
+++ World Wide Protest/FirstViewController.h
@@ -14,6 +14,7 @@
 @property(nonatomic,strong) WKWebView* webView;
 
 
+@property (strong, nonatomic) IBOutlet UIButton *backButton;
 
 
 @end
--- World Wide Protest/FirstViewController.m
+++ World Wide Protest/FirstViewController.m
@@ -23,11 +23,17 @@
     self.webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height-44-66)];
     [self.view addSubview:self.webView];
     
-    NSURL* url = [NSURL URLWithString:@"https://worldwideprotest.com"];
+    NSURL* url = [NSURL URLWithString:@"https://worldwideprotest.com/event-list"];
     NSURLRequest* request = [NSURLRequest requestWithURL:url];
     [self.webView loadRequest:request];
     
+    [self.view bringSubviewToFront:self.backButton];
+    
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)back:(id)sender {
+    [self.webView goBack];
 }
 
 
